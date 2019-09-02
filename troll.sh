e232d4599c1a8680324031ff26a101122b9b36b6
MR4N0NYM0U5 <53063518+MR4N0NYM0U5@users.noreply.github.com>
2019-09-03 06:02:46 +0700

    Add files via upload

---
+++ troll.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+#Troll Spammer
+#PERINGATAN : KESALAHAN ATAU KEJAHATAN ADALAH TANGGUNG JAWAB DARI PEMAKAI
+#GUNAKAN TOOLS UNTUK KEBAIKAN JANGAN DISALAH GUNAKAN :)
+troll(){
+    clear
+    echo Troll Spammer | lolcat
+    echo Modificated by:Mr.4N0NYM0U5 | lolcat
+    echo " =============================" | lolcat
+    sleep 2
+    echo " ==      Troll Spammer      ==" | lolcat
+    sleep 0.7
+    echo " =============================" | lolcat
+}
+load(){
+    echo -e "\n"
+    bar=" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+    barlength=${#bar}
+    i=0
+    while((i<100)); do
+        n=$((i*barlength / 100))
+        printf "\e[00;32m\r[%-${barlength}s]\e[00m" "${bar:0:n}"
+        ((i += RANDOM%5+2))
+        sleep 0.2
+    done
+}
+get_sms=$(curl -s http://zlucifer.com/api/hackbae.php?request=sms)
+get_call=$(curl -s http://zlucifer.com/api/hackbae.php?request=call)
+mulai (){
+    echo "Gunakan Troll Spammer Lagi?"
+    echo "y/n?"
+    echo
+    read lagi
+    if [ $lagi = "y" ]; then
+            spam
+    else
+            echo "Terimakasih sudah menggunakan Troll Spammer"
+    fi
+}
+#spam
+spam(){
+    clear
+    troll
+    echo
+    echo ====================== | lolcat
+    echo "[1] Spam SMS" | lolcat
+    echo ====================== | lolcat
+    echo "[2] Spam Telp"
+ | lolcat
+    echo ====================== | lolcat
+    echo "[3] Tutup Troll Project"
+ | lolcat
+    echo ====================== | lolcat
+    echo
+    echo "Silahkan dipilih:"
+    read pilih
+    if [ $pilih = "1" ]; then
+            echo "Troll Spam SMS"
+            #function spam
+            echo
+            echo "Silahkan masukan nomor telp target"
+            echo contoh 0812345678
+            read target # masukin no telp
+            echo
+            echo "Jumlah Spam?"
+            read paket
+            echo
+            echo Apakah nomor $target "dan SMS dikirim "$paket" sudah benar?"
+            echo y/n?
+            read confirm
+            echo
+            if [ $confirm = "y" ]; then
+                    load
+                    clear
+                    echo Melakukan spam SMS ke nomor $target
+                    echo
+                    echo "Jangan tutup aplikasi sebelum spam selesai"
+                    echo "========================================"
+                    target_do=$get_sms'/sms.php?nomor='$target'&paket='$paket
+                    CURL_RESPONSE=`curl -s -o /dev/null -w "%{http_code}" $target_do`
+                    echo " Gunakan tools dengan bijak"
+                    echo
+                    echo " Salam Hacker :) "
+                    echo " -Mr.4N0NYM0U5"
+                    echo "======================================="
+            else
+                    echo "Kesalahan"
+            fi
+        mulai
+    elif [ $pilih = "2" ]; then
+            echo "Troll Call Spammer"
+            #function spam
+            echo
+            echo "Silahkan masukan nomor telp target"
+            echo contoh 0812345678
+            read target # masukin no telp
+            echo
+            echo "Gunakan API Grab/Toped?"
+            echo "[1] GRAB"
+ | lolcat
+            echo =============== | lolcat
+            echo "[2] TOPED"
+ | lolcat
+            echo =============== | lolcat
+            echo "Pilih API:"
+            read api
+            if [ $api = "1" ]; then
+                  api_spam="grab"
+            else
+                  api_spam="toped"
+            fi
+            echo Apakah nomor $target dan spam menggunakan $api_spam "sudah benar?"
+            echo y/n?
+            read confirm
+            echo
+            if [ $confirm = "y" ]; then
+                  load
+                  clear
+                  echo Melakukan spam call ke nomor $target
+                  echo
+                  echo "Jangan tutup aplikasi sebelum spam selesai"
+                  echo "========================================"
+                  cek_target=`curl -s $get_call/call.php?nomor=$target"&method="$api_spam`
+                  echo -e $cek_target
+                  echo " Gunakan tools dengan bijak"
+                  echo
+                  echo " Salam Hacker "
+                  echo " -Mr.4N0NYM0U5"
+                  echo "========================================"
+            else
+                  echo Kesalahan, silahkan coba lagi
+            fi
+        mulai
+    elif [ $pilih = "3" ]; then
+        echo "Terimakasih sudah menggunakan Troll Spammer"
+        close
+    else
+        clear
+        echo "Kesalahan"
+        mulai
+    fi
+}
+close(){
+    exit
+}
+clear
+echo "Loading.."
+load
+clear
+troll
+echo Selamat datang kak, Siapa nick kaka? #tulisan keluar
+read nick #membaca yang ditulis
+clear
+troll
+echo Selamat datang $nick ":)"
+echo
+echo "Mulai troll project?"
+echo "y/n?"
+read mulai
+if [ $mulai = "y" ]; then
+      spam
+else
+      close
+fi
